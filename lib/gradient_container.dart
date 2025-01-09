73323151386a4fef62ec3092ce8c66e06bcad282
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2025-01-10 00:35:04 +0600

    using random number in stateful widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_application/roll_dice.dart';
 // import 'package:roll_dice_app/styled_text.dart';
 
 var startAlignment = Alignment.topLeft;
@@ -10,41 +11,18 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
-  void rollDice()
-  {
-    //...
-  }
-
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [color1,color2],
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset('assets/images/dice-1.png',width: 250),
-            const SizedBox(
-              height: 20,
-            ),
-            TextButton(onPressed: rollDice, 
-            style: TextButton.styleFrom(
-              padding: const EdgeInsets.only(top: 10,),
-              foregroundColor: Colors.white,
-              textStyle: const TextStyle(
-                fontSize: 28,
-              )
-            ),
-            child: const Text("Roll Dice")
-            ),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
