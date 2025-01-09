818163049aebc56a3d24b15a40610d374985fdc6
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2025-01-09 15:58:40 +0600

    styling button using padding and sized box

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -27,9 +27,20 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset('assets/images/dice-1.png',width: 250),
+            const SizedBox(
+              height: 20,
+            ),
             TextButton(onPressed: rollDice, 
+            style: TextButton.styleFrom(
+              padding: const EdgeInsets.only(top: 10,),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(
+                fontSize: 28,
+              )
+            ),
             child: const Text("Roll Dice")
             ),
           ],
